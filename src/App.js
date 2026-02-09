7ee99fd11ca46f6c7727d7b8f56a89af841f3f95
Priscillia Jimoh <priscilliajimoh7@icloud.com>
2026-02-09 20:52:37 +0000

    navbar created and mounted

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import LandingPage from "./Components/LoginSignUp/LandingPage";
+import NavBar from "./Components/NavBar/NavBar";
 
 function App() {
   return (
     <div>
+      <NavBar/>
       <LandingPage/>
     </div>
   );
---
+++ src/Components/NavBar/NavBar.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './NavBar.css';
+import fitnessLogo from '../Assets/fitness_logo.png';
+
+const NavBar = () => {
+
+    return (
+        <nav className='navbar'>
+            <img src ={fitnessLogo} alt='fitness logo' />
+            <span className='app-name'>VigoraPulse</span>
+            <div className="nav-links">
+                <a className='nav-text'>About</a>
+                <a className='nav-text'>Help</a>
+            </div>
+        </nav>
+    )
+}
+
+export default NavBar
