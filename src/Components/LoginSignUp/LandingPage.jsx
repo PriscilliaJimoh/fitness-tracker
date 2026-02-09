36ee30846088404c3daad32dedc6e6d34fb37df7
Priscillia Jimoh <priscilliajimoh7@icloud.com>
2026-02-09 10:55:24 +0000

    login/signup page with funtionality

--- src/Components/LoginSignUp/LandingPage.jsx
+++ src/Components/LoginSignUp/LandingPage.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './LandingPage.css';
 
 // import images from asset folder
@@ -7,20 +7,22 @@ import './LandingPage.css';
 
 const LoginSignUp = () => {
 
+    const [action, setAction] = useState("Sign Up");
+
     return (
         <div className='container'>
             <div className='header'>
                 <div className='text'>
-                   Sign Up?
+                    {action}
                 </div>
                 <div className='underline'>
                 </div>
             </div>
             <div className='inputs'>
-                <div className='input'>
+                {action==='Login'?<div></div>:<div className='input'>
                     {/* <img  src ='' alt=''/> */}
                     <input type='text' placeholder='Name'/>
-                </div>
+                </div>}
             </div>
             <div className='inputs'>
                 <div className='input'>
@@ -34,11 +36,11 @@ const LoginSignUp = () => {
                     <input type='password' placeholder='Password'/>
                 </div>
             </div>
-            <div className='forgot-password'>Forgot Password?{' '}
-                <span>Reset Here</span></div>
+            {action==='Sign Up'?<div></div>:<div className='forgot-password'>Forgot Password?
+                <span>Reset Here</span></div>}
             <div className='submit-container'>
-                <div className='submit'>Sign Up</div>
-                <div className='submit'>Login</div>
+                <div className={action==='Login'?'submit grey':'submit'} onClick={()=> {setAction('Sign Up')}}>Sign Up</div>
+                <div className={action==='Sign Up'?'submit grey':'submit'} onClick={(()=>{setAction('Login')})}>Login</div>
             </div>
         </div>
     )
