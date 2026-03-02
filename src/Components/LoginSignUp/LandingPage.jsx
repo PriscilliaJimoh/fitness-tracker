b0cae862ff8d1d850ee4367faf47b10224c7fdd3
Priscillia Jimoh <priscilliajimoh7@icloud.com>
2026-03-02 19:02:15 +0000

    passing data to the backend

--- src/Components/LoginSignUp/LandingPage.jsx
+++ src/Components/LoginSignUp/LandingPage.jsx
@@ -10,57 +10,69 @@ const LoginSignUp = () => {
 
     const [action, setAction] = useState("Sign Up");
 
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        // console.log(e);
+
+        const formData = new FormData(e.target);
+        // console.log(formData.keys());
+        var data = {};
+        formData.forEach((value, key) => data[key] = value);
+        var json = JSON.stringify(data);
+        console.log(json);
+    }
+
+    //
+    // const response = await fetch("https://example.org/post", {
+    //     method: "POST",
+    //     body: JSON.stringify({ username: "example" }),
+    //     // …
+    // });
+
     return (
         <div className='container'>
-            <form>
+            <form onSubmit={handleSubmit}>
+                <div className='header'>
+                    <div className='text'>
+                        {action}
+                    </div>
+                    <div className='underline'>
+                    </div>
+                </div>
+                <div className='inputs'>
+                    {action === 'Login' ? <div></div> : <div className='input'>
+                            <input type='text' name='username' placeholder='Username'/>
+                    </div>}
+                </div>
+                <div className='inputs'>
+                    <div className='input'>
+                            <input type='email' name='email' placeholder='Email Address'/>
+                    </div>
+                </div>
+                <div className='inputs'>
+                    <div className='input'>
+                            <input type='password' name='password' placeholder='Password'/>
+                    </div>
+                </div>
+                {action === 'Sign Up' ? <div></div> : <div className='forgot-password'>Forgot Password?
+                    <span>Reset Here</span></div>}
+                {action === 'Login' ? <div></div> : <div className='existing-account'>Have An Account?
+                    <span onClick={() => {
+                        setAction('Login');
+                        console.log("clicked");
+                    }}>Login Here</span></div>}
+                <div className='submit-container'>
+                    {action === 'Login' ? <button></button> :
+                        <button type="submit" className={action === 'Login' ? 'submit grey' : ' submit-button'} onClick={() => {
+                            setAction('Sign Up')
+                        }}>Submit</button>}
+                    {action === 'Sign Up' ? <button></button> :
+                        <button type="submit" className={action === 'Sign Up' ? 'submit grey' : 'submit'} onClick={(() => {
+                            setAction('Login')
+                        })}>Login</button>}
 
+                </div>
             </form>
-            <div className='header'>
-                <div className='text'>
-                    {action}
-                </div>
-                <div className='underline'>
-                </div>
-            </div>
-            <div className='inputs'>
-                {action === 'Login' ? <div></div> : <div className='input'>
-                    <form>
-                        <input type='text' placeholder='Username'/>
-                    </form>
-                </div>}
-            </div>
-            <div className='inputs'>
-                <div className='input'>
-                    <form>
-                        <input type='email' placeholder='Email Address'/>
-                    </form>
-                </div>
-            </div>
-            <div className='inputs'>
-                <div className='input'>
-                    <form>
-                        <input type='password' placeholder='Password'/>
-                    </form>
-                </div>
-            </div>
-            {action === 'Sign Up' ? <div></div> : <div className='forgot-password'>Forgot Password?
-                <span>Reset Here</span></div>}
-            {action === 'Login' ? <div></div> : <div className='existing-account'>Have An Account?
-                <span onClick={() => {
-                    setAction('Login');
-                    console.log("clicked");
-                }}>Login Here</span></div>}
-            <div className='submit-container'>
-                {action === 'Login' ? <div></div> :
-                    <div className={action === 'Login' ? 'submit grey' : 'submit-button'} onClick={() => {
-                        setAction('Sign Up')
-                    }}>Submit</div>}
-                {action === 'Sign Up' ? <div></div> :
-                    <div className={action === 'Sign Up' ? 'submit grey' : 'submit'} onClick={(() => {
-                        setAction('Login')
-                    })}>Login</div>}
-
-            </div>
         </div>
     )
 };
